8a2e44975090fc3d436e521f166071ea7922ca6c
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-02-11 14:45:59 -0600

    feat: list group users

--- providers/okta.go
+++ providers/okta.go
@@ -47,3 +47,45 @@ func NewClient(orgURL string, clientID string, jwkBytes []byte, scopes ...string
 	client := okta.NewAPIClient(config)
 	return &Client{oktaClient: client}, nil
 }
+
+type ListGroupUsersOpt func(okta.ApiListGroupUsersRequest) okta.ApiListGroupUsersRequest
+
+func WithLimit(limit int32) ListGroupUsersOpt {
+	return func(r okta.ApiListGroupUsersRequest) okta.ApiListGroupUsersRequest {
+		return r.Limit(limit)
+	}
+}
+
+func (c *Client) ListGroupUsers(ctx context.Context, groupId string, opts ...ListGroupUsersOpt) (*[]okta.GroupMember, error) {
+	query := c.oktaClient.GroupAPI.ListGroupUsers(ctx, groupId)
+	for _, opt := range opts {
+		query = opt(query)
+	}
+
+	users, resp, err := c.oktaClient.GroupAPI.ListGroupUsersExecute(query)
+	if err != nil {
+		return nil, err
+	}
+
+	for resp.HasNextPage() {
+		var nextSet []okta.GroupMember
+		resp, err = resp.Next(&nextSet)
+		users = append(users, nextSet...)
+	}
+
+	return &users, nil
+}
+
+func (c *Client) GroupByName(ctx context.Context, groupName string) (*okta.Group, error) {
+	query := c.oktaClient.GroupAPI.ListGroups(ctx).Q(groupName)
+	oktaGroups, _, err := query.Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	// okta group names are unique
+	if len(oktaGroups) != 1 {
+		return nil, fmt.Errorf("unable to find okta group %q", groupName)
+	}
+	return &oktaGroups[0], nil
+}
