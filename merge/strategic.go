a50e71bcca6bb1b2c2f130a414e6df1988042306
colinhoglund <colinhoglund@users.noreply.github.com>
2025-03-24 10:27:35 -0700

    add comments

--- merge/strategic.go
+++ merge/strategic.go
@@ -6,6 +6,8 @@ import (
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
 )
 
+// Strategic takes any number of generic objects and merges them using the strategic merge patch strategy.
+// https://github.com/kubernetes/community/blob/6690abcd6b833f46550f5eaba2ec17a9e39b35c4/contributors/devel/sig-api-machinery/strategic-merge-patch.md
 func Strategic[V any](original, modified *V, extra ...*V) (*V, error) {
 	if original == nil {
 		return nil, nil
