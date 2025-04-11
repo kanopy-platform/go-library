627f7c174c5f8040e27d64d42e49ff4e39dd2a83
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-04-11 07:41:10 -0500

    refactor: remove internal usage of `option.Get`

--- goutils/option/option.go
+++ goutils/option/option.go
@@ -2,30 +2,31 @@ package option
 
 import "fmt"
 
-func Some[T any](value T) Option[T] {
-	return Option[T]{&value}
-}
-
-func None[T any]() Option[T] {
-	return Option[T]{}
-}
-
+// Option represents a value that may or may not be present.
 type Option[T any] struct {
-	value *T
+    value *T
+}
+
+// Some creates an Option with a value.
+func Some[T any](value T) Option[T] {
+    return Option[T]{&value}
+}
+
+// None creates an empty Option.
+func None[T any]() Option[T] {
+    return Option[T]{}
 }
 
 func (o Option[T]) Filter(predicate func(val T) bool) Option[T] {
-	value, _ := o.Get()
-	if value != nil && predicate(*value) {
-		return Some(*value)
+	if o.value != nil && predicate(*o.value) {
+		return Some(*o.value)
 	}
 	return None[T]()
 }
 
 func (o Option[T]) Inspect(f func(T) Option[T]) Option[T] {
-	value, _ := o.Get()
-	if value != nil {
-		f(*value)
+	if o.value != nil {
+		f(*o.value)
 	}
 	return o
 }
@@ -38,17 +39,15 @@ func (o Option[T]) Get() (*T, error) {
 }
 
 func (o Option[T]) Or(other Option[T]) Option[T] {
-	value, _ := o.Get()
-	if value != nil {
-		return Some(*value)
+	if o.value != nil {
+		return Some(*o.value)
 	}
 	return other
 }
 
 func (o Option[T]) OrElse(f func() Option[T]) Option[T] {
-	value, _ := o.Get()
-	if value != nil {
-		return Some(*value)
+	if o.value != nil {
+		return Some(*o.value)
 	}
 	return f()
 }
@@ -58,9 +57,8 @@ func (o Option[T]) IsSome() bool {
 }
 
 func Map[S any, D any](o Option[S], f func(S) D) Option[D] {
-	value, _ := o.Get()
-	if value != nil {
-		v := f(*value)
+	if o.value != nil {
+		v := f(*o.value)
 		return Some(v)
 	}
 	return None[D]()
