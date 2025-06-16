3b8261bcadc287ea45aab1f426a4dca0e2535fb5
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-06-16 10:11:10 -0500

    cli: cobra TimeFlag type (#13)

---
+++ cli/flags.go
@@ -0,0 +1,28 @@
+package cli
+
+import (
+	"time"
+)
+
+type TimeFlag time.Time
+
+func (t *TimeFlag) String() string {
+	return time.Time(*t).Format(time.RFC3339Nano)
+}
+
+func (t *TimeFlag) Set(value string) error {
+	parsedTime, err := time.Parse(time.RFC3339Nano, value)
+	if err != nil {
+		return err
+	}
+	*t = TimeFlag(parsedTime)
+	return nil
+}
+
+func (t *TimeFlag) Type() string {
+	return "time"
+}
+
+func (t TimeFlag) Time() time.Time {
+	return time.Time(t)
+}
